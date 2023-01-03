701cac188066a81252005aac62ad6c3f53db8f67
Kamya <kamyakumari140@gmail.com>
2023-01-03 22:08:35 +0530

    adding some js

---
+++ script.js
@@ -0,0 +1,38 @@
+let boxes = Array.from(document.getElementsByClassName('box'));
+let restartBtn = document.getElementById('restartBtn');
+
+console.log(boxes);
+
+const O_TEXT = 'O';
+const X_TEXT = 'X';
+let currentPlayer = X_TEXT;
+let spaces = Array(9).fill(null);
+
+const startGame = () => {
+    boxes.forEach(box => box.addEventListener('click', boxClicked));
+}
+
+function boxClicked(e) {
+    const id = e.target.id;
+
+    if (!spaces[id]) {
+        spaces[id] = currentPlayer;
+        e.target.innerText = currentPlayer;
+
+        currentPlayer = currentPlayer == X_TEXT ? O_TEXT : X_TEXT;
+    }
+}
+
+restartBtn.addEventListener('click', restart);
+
+function restart() {
+    spaces.fill(null);
+
+    boxes.forEach( box => {
+        box.innerText = '';
+    })
+
+    currentPlayer = X_TEXT;
+}
+
+startGame();
