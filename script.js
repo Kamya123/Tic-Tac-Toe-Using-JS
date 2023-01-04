b469c72408f4eda823ae0af75e4ee9948901d5a3
Kamya <kamyakumari140@gmail.com>
2023-01-04 23:49:32 +0530

    completed changes

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ let playerText = document.getElementById('playerText');
 let boxes = Array.from(document.getElementsByClassName('box'));
 let restartBtn = document.getElementById('restartBtn');
 
+let winnerIndicator = getComputedStyle(document.body).getPropertyValue('--winning-blocks');
+
 const O_TEXT = 'O';
 const X_TEXT = 'X';
 let currentPlayer = X_TEXT;
@@ -11,20 +13,48 @@ const startGame = () => {
     boxes.forEach(box => box.addEventListener('click', boxClicked));
 }
 
+playerText.innerText = `${currentPlayer} - Turn`;
 function boxClicked(e) {
     const id = e.target.id;
 
     if (!spaces[id]) {
         spaces[id] = currentPlayer;
         e.target.innerText = currentPlayer;
-        playerText.innerText = `${currentPlayer} Turn`;
 
         if (playerHasWon() !== false) {
-            playerText.innerText = `${currentPlayer} Has WON!`
+            playerText.innerText = `${currentPlayer} Has WON!! Click Restart`;
+            let winning_blocks = playerHasWon();
+
+            winning_blocks.map(box => boxes[box].style.backgroundColor = winnerIndicator);
+
+            return;
         }
 
         currentPlayer = currentPlayer == X_TEXT ? O_TEXT : X_TEXT;
+        playerText.innerText = `${currentPlayer} - Turn`;
     }
+}
+
+const winningCombos = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+]
+
+function playerHasWon() {
+    for (const condition of winningCombos) {
+        let [a, b, c] = condition;
+
+        if (spaces[a] && (spaces[a] == spaces[b] && spaces[a] == spaces[c])) {
+            return [a,b,c];
+        }
+    }
+    return false;
 }
 
 restartBtn.addEventListener('click', restart);
@@ -34,9 +64,11 @@ function restart() {
 
     boxes.forEach( box => {
         box.innerText = '';
+        box.style.backgroundColor='';
     })
 
     currentPlayer = X_TEXT;
+    playerText.innerText = `${currentPlayer} - Turn`;
 }
 
 const themeToggleBtn = document.querySelector('.theme-toggler');
